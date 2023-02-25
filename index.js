53c68a71b30a8366f73a99d4de57f513865f7f1b
Matias Leon <matiasleon2506@gmail.com>
2023-02-24 21:05:30 -0300

    Inicio proyecto academy

---
+++ index.js
@@ -0,0 +1,10 @@
+const nombre = document.querySelector("#userName");
+const apellido = document.querySelector("#userSurname");
+const documentoNacional = document.querySelector("#userID");
+const nacionalidad = document.querySelector("#userNacionality");
+const phoneNumber = document.querySelector("#userMobile");
+const email = document.querySelector("#userEmail");
+const form = document.querySelector("#form");
+const list_Inputs = document.querySelectorAll(".input-form");
+
+console.log (list_Inputs)
