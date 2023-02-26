7d5165132fa8610d5bde50a539ee27ab01a5a912
Matias Leon <matiasleon2506@gmail.com>
2023-02-25 23:52:48 -0300

    Continuo con js, agrego primera validación del formulario, me quede con un error de innerHTML value null a arreglar

--- js/index.js
+++ js/index.js
@@ -4,7 +4,20 @@ const documentoNacional = document.querySelector("#userID");
 const nacionalidad = document.querySelector("#userNacionality");
 const phoneNumber = document.querySelector("#userMobile");
 const email = document.querySelector("#userEmail");
-const form = document.querySelector("#form");
+const form = document.querySelector(".form");
 const list_Inputs = document.querySelectorAll(".input-form");
 
-console.log (list_Inputs)
+
+
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    if(nombre.value.lenght < 1 || nombre.value.trim() ==""){
+        let entrada = document.getElementById(".mensajeError");
+        entrada.innerHTML = "Nombre no valido";
+    }
+
+    if(apellido.value.lenght < 1 || nombre.value.trim() ==""){
+        console.error("Error")
+    }
+})
+
