58a1b09922a815d2ab6d0745f2713c43102a525b
Matias Leon <matiasleon2506@gmail.com>
2023-02-26 23:57:08 -0300

    Agrego favicon, y funciones y condicionales para la validación del formulario, no encontre un evento que permita agregar información nueva para experiencia y habilidades

--- js/index.js
+++ js/index.js
@@ -4,20 +4,82 @@ const documentoNacional = document.querySelector("#userID");
 const nacionalidad = document.querySelector("#userNacionality");
 const phoneNumber = document.querySelector("#userMobile");
 const email = document.querySelector("#userEmail");
+const experience = document.querySelector("#experience");
+const tasks_experience = document.querySelector("#tasksJobs");
+const habilities = document.querySelector("#habilities");
 const form = document.querySelector(".form");
+const T_and_C = document.querySelector("#thermsAndConditions");
 const list_Inputs = document.querySelectorAll(".input-form");
 
 
 
 form.addEventListener("submit", (e) =>{
     e.preventDefault();
+    let condition = validacionForm();
+    if (condition == true){
+        enviarFormulario();
+    }
+});
+
+
+function validacionForm(){
+    let condition = true;
+    list_Inputs.forEach(entrada =>{
+        entrada.lastElementChild.innerHTML = "";
+    })
+
     if(nombre.value.lenght < 1 || nombre.value.trim() ==""){
-        let entrada = document.getElementById(".mensajeError");
-        entrada.innerHTML = "Nombre no valido";
+        mostrarMensajeError("userName", "*Campo obligatorio");
+        condition = false;
     }
 
     if(apellido.value.lenght < 1 || nombre.value.trim() ==""){
-        console.error("Error")
+        mostrarMensajeError("userSurname", "*Campo obligatorio");
+        condition = false;
+
     }
-})
+
+    if (documentoNacional == isNaN || documentoNacional.value.lenght < 8 || documentoNacional.value.trim() ==""){
+        mostrarMensajeError ("userID", "*Campo obligatorio")
+        condition = false;
+    }
+
+
+    if (nacionalidad.value.lenght < 1 || nacionalidad.value.trim() ==""){
+        mostrarMensajeError ("userNacionality", "*Campo obligatorio")
+        condition = false;
+    }
+    
+    if (phoneNumber.value.lenght < 1 || phoneNumber == isNaN || phoneNumber.value.trim() ==""){
+        mostrarMensajeError ("userMobile", "*Campo obligatorio")
+        condition = false;
+    }
+
+    if (email.value.lenght < 1 || email.value.trim() ==""){
+        mostrarMensajeError ("userEmail" , "*Campo obligatorio")
+        condition = false;
+    }
+
+    if (!T_and_C.checked){
+        mostrarMensajeError("thermsAndConditions", "Acepte terminos y condiciones")
+        condition = false;
+    } else {
+        mostrarMensajeError("thermsAndConditions", "")
+    }
+
+    return condition;
+}
+
+function mostrarMensajeError(claseInput, mensaje){
+    let entrada = document.querySelector(`.${claseInput}`);
+    entrada.lastElementChild.innerHTML = mensaje;
+}
+
+
+function enviarFormulario(){
+    form.reset();
+    form.lastElementChild.innerHTML = "¡Registro completado!"
+}
+
+
 
